2110c470f5318594e1a9f3f0adc4ef205c694503
JUSTIN <jchang@crystal.csus.org>
2016-07-31 14:14:38 -0500

    camera class

--- Space Game/src/camera/Camera.java
+++ Space Game/src/camera/Camera.java
@@ -1,13 +1,6 @@
 package camera;
 
-import acm.graphics.GRect;
-
 public class Camera extends GRect 
 {
-	
-	public Camera(double x, double y, double width, double height)
-	{
-		super(x, y, width, height);
-	}
-	
+
 }
