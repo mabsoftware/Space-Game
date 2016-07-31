8b59664247a44b80ceb6d7582743c03315b467ee
MAB <maxabowman@gmail.com>
2016-07-31 10:00:15 -0500

    Began work on the projectile class.

--- Space Game/src/player/Projectile.java
+++ Space Game/src/player/Projectile.java
@@ -1,6 +1,25 @@
 package player;
 
-public class Projectile
-{
+import java.awt.Image;
+import acm.graphics.GImage;
+import physics.Vector;
 
+public class Projectile extends GImage
+{
+	private double myX;
+	private double myY;
+	private double missleXSpeed;
+	private double missleYSpeed;
+	private Vector myVector;
+	private Image myImage;
+	
+	public Projectile(Image image, double startX, double startY, Player p)
+	{
+		super(image, startX, startY); // create a GImage.
+		myImage = image;
+		myX = startX;
+		myY = startY; // create instance variables.
+		missleXSpeed = 5;
+		missleYSpeed = 0;
+	}
 }
