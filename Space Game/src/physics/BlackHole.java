98785a0fd6d6688de76c26231e62fdce7c0bdb78
Max Bowman <maxabowman@gmail.com>
2016-07-31 10:24:34 -0500

    I worked on BlackHole.java and GravityObject.java.

--- Space Game/src/physics/BlackHole.java
+++ Space Game/src/physics/BlackHole.java
@@ -1,6 +1,12 @@
 package physics;
 
+import java.awt.Image;
+
 public class BlackHole extends GravityObject
 {
-	// black hole class
+	public BlackHole(Image image, double startX, double startY)
+	{
+		super(image, startX, startY, 0, 0); // black holes don't move in the game.
+		
+	}
 }
--- Space Game/src/physics/GravityObject.java
+++ Space Game/src/physics/GravityObject.java
@@ -1,6 +1,18 @@
 package physics;
 
-public class GravityObject
-{
+import acm.graphics.GImage;
+import java.awt.Image;
+import physics.Vector;
 
+public class GravityObject extends GImage
+{
+	private Vector myVector; // gravity objects move too.
+	
+	public GravityObject(Image image, double startX, double startY, double xVel, double yVel)
+	{
+		super(image, startX, startY);
+		
+		myVector.setXComponent(xVel);
+		myVector.setYComponent(yVel); // set GravityObject's vector.
+	}
 }
