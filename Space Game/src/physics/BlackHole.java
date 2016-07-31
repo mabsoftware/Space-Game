72ac2ebd5760537a48f9cdfe051f331a0a4dd3a9
Max Bowman <maxabowman@gmail.com>
2016-07-31 10:43:59 -0500

    I updated BlackHole.java.

--- Space Game/src/physics/BlackHole.java
+++ Space Game/src/physics/BlackHole.java
@@ -4,6 +4,8 @@ import java.awt.Image;
 
 public class BlackHole extends GravityObject
 {
+	private final double MULTIPLIER = 100;
+	
 	public BlackHole(Image image, double startX, double startY)
 	{
 		super(image, startX, startY, 0, 0); // black holes don't move in the game.
