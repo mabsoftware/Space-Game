d634f73f2ddd87f44e9821e79b8dedbc5c4b725f
Max Bowman <maxabowman@gmail.com>
2016-07-31 10:33:05 -0500

    I made player extend a GPolygon.
    The GPolygon class will allow us to easily transform (it takes care of
    matrix math for transformations) and also allow us to make our hit box
    better (with the .contains method).

--- Space Game/src/player/Player.java
+++ Space Game/src/player/Player.java
@@ -1,16 +1,16 @@
 package player;
 
-import acm.graphics.GImage;
+import acm.graphics.GPolygon;
 import java.awt.Image;
 import physics.Vector;
 
-public class Player extends GImage
+public class Player extends GPolygon
 {
 	private Vector myVector;
 	
 	public Player(Image image, double startX, double startY, double xVel, double yVel)
 	{
-		super(image, startX, startY); // create player GImage.
+		super(startX, startY); // create player GImage.
 		
 		myVector.setXComponent(xVel);
 		myVector.setYComponent(yVel); // set initial player vector.
