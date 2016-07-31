911ad00926185f112ff4c8db3ae3eb986facc1d2
MAB <maxabowman@gmail.com>
2016-07-31 10:19:31 -0500

    Updated Projectile.java.

--- Space Game/src/player/Projectile.java
+++ Space Game/src/player/Projectile.java
@@ -6,20 +6,17 @@ import physics.Vector;
 
 public class Projectile extends GImage
 {
-	private double myX;
-	private double myY;
 	private final double missleSpeed = 5;
 	private Vector myVector;
-	private Image myImage;
 	
 	public Projectile(Image image, double startX, double startY, Player p)
 	{
 		super(image, startX, startY); // create a GImage.
-		myImage = image;
-		myX = startX;
-		myY = startY; // create instance variables.
 		
 		myVector = p.getVector();
 		myVector.multiplyByScalar(missleSpeed); // player's vector + missle's speed ( a scalar ).
 	}
+	
+	public Vector getVector()
+	{ return myVector; }
 }
