6bfd4a2c2c3747d835850190e5eae8afe9ce061d
Max Bowman <maxabowman@gmail.com>
2016-07-31 10:50:14 -0500

    AI package and class created.

---
+++ Space Game/src/ai/Enemy.java
@@ -0,0 +1,16 @@
+package ai;
+
+import acm.graphics.GPolygon;
+
+public class Enemy extends GPolygon
+{
+	public Enemy(double startX, double startY)
+	{
+		super(startX, startY);
+	}
+	
+	public void rotate(double degrees)
+	{
+		this.rotate(degrees);
+	}
+}
