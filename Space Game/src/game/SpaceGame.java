3fe8ca5537bef01cbb8f7c744d679f995562c274
Max Bowman <maxabowman@gmail.com>
2016-07-31 13:02:49 -0500

    Player can rotate.

--- Space Game/src/game/SpaceGame.java
+++ Space Game/src/game/SpaceGame.java
@@ -6,10 +6,13 @@
 package game;
 
 import acm.program.GraphicsProgram;
+import player.Player;
+import java.awt.event.KeyEvent;
 
 public class SpaceGame extends GraphicsProgram 
 {
 	private boolean running; // game state.
+	private Player player = new Player(100, 100, 0, 0);
 	
 	public static void main(String args[])
 	{
@@ -21,6 +24,10 @@ public class SpaceGame extends GraphicsProgram
 		this.setSize(320 * 2, 240 * 2);
 		this.setTitle("Space Game");
 		running = true;
+		
+		this.add(player); // add the player to the window.
+		
+		this.addKeyListeners();
 	}
 	
 	public void run()
@@ -29,5 +36,18 @@ public class SpaceGame extends GraphicsProgram
 		{
 			
 		} // game loop.
+	}
+	
+	public void keyPressed(KeyEvent k)
+	{
+		System.out.println(k);
+		if (k.getKeyChar() == 'a')
+		{
+			player.rotate(5);
+		}
+		else if (k.getKeyChar() == 'd')
+		{
+			player.rotate(-5);
+		}
 	}
 }
--- Space Game/src/player/Player.java
+++ Space Game/src/player/Player.java
@@ -1,21 +1,35 @@
 package player;
 
 import acm.graphics.GPolygon;
-import java.awt.Image;
+import acm.graphics.GPoint;
 import physics.Vector;
 
 public class Player extends GPolygon
 {
 	private Vector myVector;
 	
-	public Player(Image image, double startX, double startY, double xVel, double yVel)
+	public Player(double startX, double startY, double xVel, double yVel)
 	{
 		super(startX, startY); // create player GImage.
 		
-		myVector.setXComponent(xVel);
-		myVector.setYComponent(yVel); // set initial player vector.
+		myVector = new Vector(xVel, yVel); // set initial player vector.
+
+		this.addPolarEdge(10, 0);
+		this.addPolarEdge(7, 60);
+		this.addPolarEdge(10, 120);
+		//this.addPolarEdge(10, 180);
+		this.addPolarEdge(10, 240);
+		this.addPolarEdge(7, 300);
+		//this.addPolarEdge(10, 360);
+		
+		
+		
+		
 	}
 	
 	public Vector getVector()
 	{ return myVector; }
+	
+	public void rotateShip(double degrees)
+	{ this.rotate(degrees); }
 }
