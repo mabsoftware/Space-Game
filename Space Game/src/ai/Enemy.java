3d0acbc589c730ad34f1577aae8340b1b4b75897
Zain <Zain@0.3.17.185>
2016-08-01 14:01:45 -0500

    Added setAngle().

--- Space Game/src/ai/Enemy.java
+++ Space Game/src/ai/Enemy.java
@@ -4,13 +4,33 @@ import acm.graphics.GPolygon;
 
 public class Enemy extends GPolygon
 {
+	
+	private double angle;
+	
 	public Enemy(double startX, double startY)
 	{
 		super(startX, startY);
+		angle = 0;
 	}
 	
-	public void rotate(double degrees)
+	public void setAngle(double newAngle)
 	{
-		this.rotate(degrees);
-	} // rotates enemy.
+		double rotateAngle = newAngle - angle;
+		this.rotate(rotateAngle);
+		angle = newAngle;
+		while (angle >= 360)
+		{
+			angle -= 360;
+		}
+		while (angle < 0)
+		{
+			angle += 360;
+		}
+		
+	} // rotates enemy counterclockwise.
+	
+	public double getAngle()
+	{
+		return angle;
+	}
 }
