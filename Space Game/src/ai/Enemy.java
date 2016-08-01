b1bfb23496f0cb093501e1d24cfdf8a8b3a030d8
Zain <Zain@0.3.17.185>
2016-08-01 16:03:06 -0500

    Made enemies move towards nearest player. Not tested.

--- Space Game/src/ai/Enemy.java
+++ Space Game/src/ai/Enemy.java
@@ -1,22 +1,34 @@
 package ai;
-//
+
 import acm.graphics.GPolygon;
+import player.Player;
 
 public class Enemy extends GPolygon
 {
-	
+	private double myX;
+	private double myY;
+	private double distance;
+	private Player target;
+	private Player[] myPlayers;
 	private double angle;
+	private double xVel;
+	private double yVel;
 	
-	public Enemy(double startX, double startY)
+	public Enemy(double startX, double startY, Player[] players)
 	{
 		super(startX, startY);
 		angle = 0;
+		myX = startX;
+		myY = startY;
+		myPlayers = players;
+		target = myPlayers[0];
+		distance = Math.sqrt(Math.pow(myX - target.getXUniverse(), 2) + Math.pow(myY - target.getYUniverse(), 2));
 	}
 	
 	public void setAngle(double newAngle)
 	{
 		double rotateAngle = newAngle - angle;
-		this.rotate(rotateAngle);
+		rotate(rotateAngle);
 		angle = newAngle;
 		while (angle >= 360)
 		{
@@ -27,10 +39,33 @@ public class Enemy extends GPolygon
 			angle += 360;
 		}
 		
-	} // rotates enemy counterclockwise.
+	}
 	
-	public double getAngle()
+	public void findTarget()
 	{
-		return angle;
+		for (int i = 0; i < myPlayers.length; i++)
+		{
+			if (Math.sqrt(Math.pow(myX - target.getXUniverse(), 2) + Math.pow(myY - target.getYUniverse(), 2)) < distance)
+			target = myPlayers[i];
+			distance = Math.sqrt(Math.pow(myX - target.getXUniverse(), 2) + Math.pow(myY - target.getYUniverse(), 2));
+		}	
+	}
+	
+	public void followTarget()
+	{
+		angle = Math.atan2(Math.abs(target.getYUniverse() - myY), Math.abs(target.getXUniverse() - myX));
+		if (angle < 0)
+		{
+			angle += 180;
+		}
+		setAngle(angle);
+		xVel = Math.cos(Math.toRadians(angle));
+		yVel = Math.sin(Math.toRadians(angle));
+	}
+	
+	public void move()
+	{
+		myX += xVel;
+		myY += yVel;
 	}
 }
