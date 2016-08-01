620986cb18738183dc9cd7672f8185e9ac50a750
Zain <Zain@0.3.17.185>
2016-08-01 11:53:21 -0500

    Added an AI class and a method to find the closest player.

---
+++ Space Game/src/ai/AI.java
@@ -0,0 +1,33 @@
+package ai;
+
+import player.Player;
+
+public class AI
+{
+	private double myX;
+	private double myY;
+	private double distance;
+	private Player target;
+	private Player[] myPlayers;
+	
+	public AI(double x, double y, Player[] players)
+	{
+		myX = x;
+		myY = y;
+		myPlayers = players;
+		target = myPlayers[0];
+		distance = Math.sqrt(Math.pow(myX - target.getX(), 2) + Math.pow(myY - target.getY(), 2));
+	}
+	
+	public void findTarget()
+	{
+		for (int i = 0; i < myPlayers.length; i++)
+		{
+			if (Math.sqrt(Math.pow(myX - target.getX(), 2) + Math.pow(myY - target.getY(), 2)) < distance)
+			target = myPlayers[i];
+			distance = Math.sqrt(Math.pow(myX - target.getX(), 2) + Math.pow(myY - target.getY(), 2));
+		}
+		
+	}
+}
+
