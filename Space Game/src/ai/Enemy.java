0417e0549619763107d2ce9241ffa289edc6ef38
Zain <Zain@0.3.17.185>
2016-08-02 09:38:11 -0500

    Improved formatting and worked on attacking.

--- Space Game/src/ai/Enemy.java
+++ Space Game/src/ai/Enemy.java
@@ -2,39 +2,87 @@ package ai;
 
 import acm.graphics.GPolygon;
 import java.awt.Color;
+import player.Laser;
 import player.Player;
+import player.Projectile;
 import physics.Vector;
 
 public class Enemy extends GPolygon
 {
-	
+
 	private double distance;
 	private Player target;
 	private Player[] myPlayers;
 	private double angle;
-	
+	private int type;
 	private Vector myVector;
-	
+	private Projectile missile;
+	private Laser laser;
+
 	public Enemy(double startX, double startY, double xVel, double yVel, Player[] players)
 	{
 		super(startX, startY);
-		
+
 		this.addEdge(0, -10);
 		this.addEdge(-8, 5);
 		this.addEdge(8, 5);
 		this.setFillColor(Color.YELLOW);
 		this.setColor(Color.YELLOW);
 		this.setFilled(true);
-		
+
 		myVector = new Vector(xVel, yVel);
-		
+
 		angle = 0;
 		myPlayers = players;
 		target = myPlayers[0];
 		distance = Math.sqrt(Math.pow(this.getX() - target.getXUniverse(), 2) + Math.pow(this.getY() - target.getYUniverse(), 2));
+		if ((int) (Math.random() * 4) == 0)
+		{
+			type = 0;
+		}
+		else
+		{
+			type = 1;
+		}
 	}
-	
-	public void setAngle(double newAngle)
+
+	public void action()
+	{
+		findTarget();
+		followTarget();
+		if (distance < 300)
+		{
+			fire();
+		}
+		else		
+		{
+			move();
+		}
+	}
+
+	private void findTarget()
+	{
+		for (int i = 0; i < myPlayers.length; i++)
+		{
+			if (Math.sqrt(Math.pow(this.getX() - target.getXUniverse(), 2) + Math.pow(this.getY() - target.getYUniverse(), 2)) < distance)
+			{
+				target = myPlayers[i];
+				distance = Math.sqrt(Math.pow(this.getX() - target.getXUniverse(), 2) + Math.pow(this.getY() - target.getYUniverse(), 2));
+			}
+		}	
+	}
+
+	private void followTarget()
+	{
+		angle = Math.atan2(Math.abs(target.getYUniverse() - this.getY()), Math.abs(target.getXUniverse() - this.getY()));
+		if (angle < 0)
+		{
+			angle += 180;
+		}
+		setAngle(angle);
+	}
+
+	private void setAngle(double newAngle)
 	{
 		double rotateAngle = newAngle - angle;
 		rotate(rotateAngle);
@@ -47,33 +95,27 @@ public class Enemy extends GPolygon
 		{
 			angle += 360;
 		}
-		
 	}
 	
-	public void findTarget()
+	private void move()
 	{
-		for (int i = 0; i < myPlayers.length; i++)
-		{
-			if (Math.sqrt(Math.pow(this.getX() - target.getXUniverse(), 2) + Math.pow(this.getY() - target.getYUniverse(), 2)) < distance)
-			target = myPlayers[i];
-			distance = Math.sqrt(Math.pow(this.getX() - target.getXUniverse(), 2) + Math.pow(this.getY() - target.getYUniverse(), 2));
-		}	
-	}
-	
-	public void followTarget()
-	{
-		angle = Math.atan2(Math.abs(target.getYUniverse() - this.getY()), Math.abs(target.getXUniverse() - this.getY()));
-		if (angle < 0)
-		{
-			angle += 180;
-		}
-		setAngle(angle);
 		myVector.setXComponent(myVector.getXComponent() + Math.sin(Math.toRadians(angle)));
 		myVector.setYComponent(myVector.getYComponent() + Math.cos(Math.toRadians(angle)));
+		move(myVector.getXComponent(), myVector.getYComponent());
 	}
 	
-	public void move()
+	private void fire()
 	{
-		this.move(myVector.getXComponent(), myVector.getYComponent());
+		if (type == 0)
+		{
+//			missile = new Projectile();
+		}
+		else
+		{
+//			laser = new Laser();
+		}
 	}
+	
 }
+
+
