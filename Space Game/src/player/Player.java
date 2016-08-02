edd2520ca4ab33fa1433b5652287d663b69b7efe
Zain <Zain@0.3.17.185>
2016-08-02 13:52:04 -0500

    Made visuals smoother.

--- Space Game/src/player/Player.java
+++ Space Game/src/player/Player.java
@@ -106,13 +106,13 @@ public class Player extends GPolygon
 	{
 		if (left)
 		{
-			this.rotate(5);
-			angle -= 5;
+			this.rotate(1);
+			angle -= 1;
 		}
 		else if (right)
 		{
-			this.rotate(-5);
-			angle += 5;
+			this.rotate(-1);
+			angle += 1;
 		}
 	} // monitor user - I know the angles look backwards, but the rotate function is different.
 
