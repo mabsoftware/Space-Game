03e926a4128a21f59dd2821d3499677257988737
JUSTIN <jchang@crystal.csus.org>
2016-08-02 14:48:32 -0500

    laser

--- Space Game/src/player/Laser.java
+++ Space Game/src/player/Laser.java
@@ -5,13 +5,28 @@ import acm.graphics.GLine;
 public class Laser extends GLine
 {
 	
-	private double myX;
-	private double myY;
+	private double xUniverse;
+	private double yUniverse;
 	private double myAngle;
-	private Laser laser;
 	
 	public Laser(Player p)
 	{
 		super(p.getX() * Math.cos(p.getAngle()) - p.getY() * Math.sin(p.getAngle()), p.getX() * Math.sin(p.getAngle()) + p.getY() * Math.cos(p.getAngle()), p.getX() * Math.cos(p.getAngle()) - p.getY() * Math.sin(p.getAngle()) - p.getX(), p.getX() * Math.sin(p.getAngle()) + p.getY() * Math.cos(p.getAngle()) - p.getY());
+	}
+
+	public double getXUniverse() {
+		return xUniverse;
+	}
+
+	public void setXUniverse(double xUniverse) {
+		this.xUniverse = xUniverse;
+	}
+
+	public double getYUniverse() {
+		return yUniverse;
+	}
+
+	public void setYUniverse(double yUniverse) {
+		this.yUniverse = yUniverse;
 	}
 }
