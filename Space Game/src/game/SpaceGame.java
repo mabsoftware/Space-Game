d2a2340c53e19e5d9d2f7ca9d4d27f487ae6eb86
Zain <Zain@0.3.17.185>
2016-08-02 11:30:25 -0500

    dsfs

--- Space Game/src/game/SpaceGame.java
+++ Space Game/src/game/SpaceGame.java
@@ -9,6 +9,7 @@ package game;
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import ai.Enemy;
+import player.Laser;
 import player.Player;
 import java.awt.event.KeyEvent;
 import java.util.Iterator;
@@ -30,6 +31,7 @@ public class SpaceGame extends GraphicsProgram
 	private double xUniverse;
 	private double yUniverse;
 	private Enemy[] enemies;
+	private Laser laser;
 	
 	public static void main(String args[])
 	{
@@ -77,7 +79,6 @@ public class SpaceGame extends GraphicsProgram
 		enemies = new Enemy[1];
 		enemies[0] = new Enemy(this.getWidth() / 3, this.getHeight() / 3, 0, 0, player, otherPlayers);
 		this.add(enemies[0]);
-		
 		// User input and output is initialized here.
 		this.addKeyListeners();
 	}
@@ -107,6 +108,7 @@ public class SpaceGame extends GraphicsProgram
 			{
 				star.move(-player.getVector().getXComponent(), -player.getVector().getYComponent());
 			}
+			enemies[0].action();
 				
 			// Clock tick
 			pause(100);
@@ -122,7 +124,6 @@ public class SpaceGame extends GraphicsProgram
 			player.getXUniverse();
 		}
 	}
-	
 	
 	// Sets screen coordinates of all objects based on Universe coordinates
 	public void draw() 
