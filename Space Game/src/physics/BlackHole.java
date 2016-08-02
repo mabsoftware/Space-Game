9165e189fd6770359386427fd522fe13f941bbe6
Zain <Zain@0.3.17.185>
2016-08-02 17:05:42 -0500

    Bugfixes

--- Space Game/src/physics/BlackHole.java
+++ Space Game/src/physics/BlackHole.java
@@ -1,14 +1,15 @@
 package physics;
 
 import java.awt.Image;
+import game.SpaceGame;
 
 public class BlackHole extends GravityObject
 {
 	private static double multiplier = 100;
 	
-	public BlackHole(String image, double xUniverse, double yUniverse) // What are xu and yu? xuniverse and yuniverse
+	public BlackHole(String image, double xUniverse, double yUniverse, SpaceGame game)
 	{
-		super(image, 0, 0, 0, multiplier, xUniverse, yUniverse); // black holes don't move in the game, and you can't see them.
+		super(image, 0, 0, 0, multiplier, xUniverse, yUniverse, game); // black holes don't move in the game, and you can't see them.
 		
 	}
 } // extends Gravity Object, exactly the same except invisible and way more gravity.
