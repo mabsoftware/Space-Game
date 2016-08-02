2a8d8f14bec6de9a2e2fbf49f5947c4ae2a0483b
Zain <Zain@0.3.17.185>
2016-08-02 10:22:16 -0500

    Started working on lasers.

--- Space Game/src/player/Laser.java
+++ Space Game/src/player/Laser.java
@@ -1,6 +1,22 @@
 package player;
 
-public class Laser
-{
+import acm.graphics.GLine;
 
+public class Laser extends GLine
+{
+	
+	private double myX;
+	private double myY;
+	private double myAngle;
+	
+	public Laser(double startX, double startY, double angle)
+	{
+		super(startX, startY, startX + 50, startY + 50);
+		myX = startX;
+		myY = startY;
+		myAngle = angle;
+	}
+	
+	
 }
+
