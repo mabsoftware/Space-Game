cb05d3b37b9183b49c64f65d5f6e828f41bb6e81
Max Bowman <maxabowman@gmail.com>
2016-08-03 18:22:07 -0500

    Finished Button.java and ButtonLabel.java. ButtonLabel.java is just
    a helper class.

--- Space Game/src/ui/Button.java
+++ Space Game/src/ui/Button.java
@@ -2,17 +2,21 @@ package ui;
 
 import acm.graphics.GRect;
 import java.awt.Color;
+import game.SpaceGame;
 
 public class Button extends GRect
 {
 	private String label;
 	
-	public Button(String s, Color c, double x, double y, double width, double height)
+	public Button(String s, Color c, double x, double y, double width, double height, SpaceGame game)
 	{
 		super(x, y, width, height);
 		this.setColor(Color.GREEN);
 		this.setFillColor(c);
 		this.setFilled(true);
+		
+		game.add(this);
+		game.add(new ButtonLabel(this));
 		
 		label = s;
 	}
--- Space Game/src/ui/ButtonLabel.java
+++ Space Game/src/ui/ButtonLabel.java
@@ -1,11 +1,15 @@
 package ui;
 
 import acm.graphics.GLabel;
+import java.awt.Color;
 
 public class ButtonLabel extends GLabel
 {
 	public ButtonLabel(Button a)
 	{
-		super(a.getLabel(), a.getX(), a.getY());
+		super(a.getLabel());
+		this.setFont("fantasy-bold-48");
+		this.setLocation(a.getX() + ((a.getWidth() - this.getWidth()) / 2), a.getY() + (a.getHeight() - this.getHeight()) / 2);
+		this.setColor(Color.WHITE);
 	}
 }
