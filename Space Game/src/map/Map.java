40e4797085869b7602e5f5d5d3b3370fc78f6ec9
Zain <Zain@0.3.17.185>
2016-08-03 10:51:20 -0500

    Planet generation changed

--- Space Game/src/map/Map.java
+++ Space Game/src/map/Map.java
@@ -13,7 +13,7 @@ public class Map
 		map1 = new double[100][6];
 		for (int i = 0; i < map1.length; i++)
 		{
-			this.setRow(i, Math.random() * 200, Math.random() * 5, Math.random() * 5, Math.random() * 2 + 1, Math.random() * 10000 - 5000, Math.random() * 10000 - 5000);
+			this.setRow(i, Math.random() * 126 + 50, Math.random() * 5, Math.random() * 5, Math.random() * 1.5 + 1, Math.random() * 10000 - 5000, Math.random() * 10000 - 5000);
 		}
 	}
 
